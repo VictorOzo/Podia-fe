22938ed38a14b2a22250c1a852c373e9665be540
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-09-03 22:20:19 +0100

    navbar created

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,5 +1,30 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div className='nav flex '>
+			<div className='navlinks'>
+				<Link to='/'>
+					<span>PODIA</span>
+				</Link>
+				<ul>
+					<Link to='features'>Features</Link>
+					<Link to='examples'>Examples</Link>
+					<Link to='pricing'>Pricing</Link>
+					<Link to='demo'>Demo</Link>
+					<Link to='resources'>Resources</Link>
+				</ul>
+			</div>
+			<div className='navbtn'>
+				<Link to='login'>
+					<button>Login</button>
+				</Link>
+				<Link to='signup'>
+					<button>Sign Up</button>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
