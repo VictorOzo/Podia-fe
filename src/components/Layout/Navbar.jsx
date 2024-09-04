b181799c53a9384c4e92c514cf9305ce205019b4
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-09-04 20:07:29 +0100

    styles added to navbar

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,29 +1,34 @@
 import { Link } from 'react-router-dom';
+import '../styles/navbar.css';
 
 const Navbar = () => {
 	return (
-		<div className='nav flex '>
-			<div className='navlinks'>
-				<Link to='/'>
-					<span>PODIA</span>
-				</Link>
-				<ul>
-					<Link to='features'>Features</Link>
-					<Link to='examples'>Examples</Link>
-					<Link to='pricing'>Pricing</Link>
-					<Link to='demo'>Demo</Link>
-					<Link to='resources'>Resources</Link>
-				</ul>
+		<nav>
+			<div className='container px-4 py-[10px] mx-auto'>
+				<div className='flex items-center justify-between nav'>
+					<div className='navlinks'>
+						<ul className='p-[12px]'>
+							<Link to='/'>
+								<span>podia</span>
+							</Link>
+							<Link to='features'>Features</Link>
+							<Link to='examples'>Examples</Link>
+							<Link to='pricing'>Pricing</Link>
+							<Link to='demo'>Demo</Link>
+							<Link to='resources'>Resources</Link>
+						</ul>
+					</div>
+					<div className='navbtn'>
+						<Link to='login'>
+							<button>Login</button>
+						</Link>
+						<Link to='signup'>
+							<button>Sign Up</button>
+						</Link>
+					</div>
+				</div>
 			</div>
-			<div className='navbtn'>
-				<Link to='login'>
-					<button>Login</button>
-				</Link>
-				<Link to='signup'>
-					<button>Sign Up</button>
-				</Link>
-			</div>
-		</div>
+		</nav>
 	);
 };
 
