5a0e50128a3166a722e78d116ba5ca0ab4f17506
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-09-04 21:50:22 +0100

    proper styles for navbar added

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,16 +1,15 @@
 import { Link } from 'react-router-dom';
+
 import '../styles/navbar.css';
 
 const Navbar = () => {
 	return (
 		<nav>
-			<div className='container px-4 py-[10px] mx-auto'>
+			<div className='container px-6 py-7 mx-auto'>
 				<div className='flex items-center justify-between nav'>
 					<div className='navlinks'>
-						<ul className='p-[12px]'>
-							<Link to='/'>
-								<span>podia</span>
-							</Link>
+						<ul>
+							<Link to='/'>podia</Link>
 							<Link to='features'>Features</Link>
 							<Link to='examples'>Examples</Link>
 							<Link to='pricing'>Pricing</Link>
@@ -19,12 +18,8 @@ const Navbar = () => {
 						</ul>
 					</div>
 					<div className='navbtn'>
-						<Link to='login'>
-							<button>Login</button>
-						</Link>
-						<Link to='signup'>
-							<button>Sign Up</button>
-						</Link>
+						<Link to='login'>Login</Link>
+						<Link to='signup'>Sign Up </Link>
 					</div>
 				</div>
 			</div>
