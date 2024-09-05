ab0eda9305e7b032e764e1e048af97b5c9a9c9bd
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-09-05 22:23:08 +0100

    tablet responsiveness added

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,20 +1,14 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 import hamburger from '../images/hamburger.png';
 import '../styles/navbar.css';
 
 const Navbar = () => {
-	const [isOpen, setIsOpen] = useState(false);
-
-	const toggleMenu = () => {
-		console.log('clicked!');
-	};
 	return (
 		<nav>
 			<div className='container px-6 mx-auto py-7'>
 				<div className='flex items-center justify-between nav '>
 					<div className='navlinks '>
-						<Link to='/' className='lg:hidden md:hidden '>
+						<Link to='/' className='hidden '>
 							podia
 						</Link>
 						<ul>
@@ -30,12 +24,8 @@ const Navbar = () => {
 						<Link to='login'>Login</Link>
 						<Link to='signup'>Sign Up </Link>
 					</div>
-					<div className='lg:hidden hamburger'>
-						<img
-							src={hamburger}
-							className='w-[20px]'
-							onClick={toggleMenu}
-						/>
+					<div className='hamburger'>
+						<img src={hamburger} className='w-[20px]' />
 					</div>
 				</div>
 			</div>
