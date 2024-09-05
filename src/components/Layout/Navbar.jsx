7c8e7c23226ad50cdf62607aafdc61262bd974cf
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-09-05 21:07:39 +0100

    hamburger menu added

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
-
+import { useState } from 'react';
+import hamburger from '../images/hamburger.png';
 import '../styles/navbar.css';
 
 const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
 	return (
 		<nav>
-			<div className='container px-6 py-7 mx-auto'>
-				<div className='flex items-center justify-between nav'>
-					<div className='navlinks'>
+			<div className='container px-6 mx-auto py-7'>
+				<div className='flex items-center justify-between nav '>
+					<div className='navlinks '>
 						<ul>
 							<Link to='/'>podia</Link>
 							<Link to='features'>Features</Link>
@@ -20,6 +26,9 @@ const Navbar = () => {
 					<div className='navbtn'>
 						<Link to='login'>Login</Link>
 						<Link to='signup'>Sign Up </Link>
+					</div>
+					<div className='lg:hidden hamburger'>
+						<img src={hamburger} className='w-[20px]' />
 					</div>
 				</div>
 			</div>
